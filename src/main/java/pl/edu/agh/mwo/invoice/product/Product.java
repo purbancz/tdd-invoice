534c51fae13145957b23238f5e9cc391bcaf1b70
Student14 <Student14@S7N-PROGLANG-30.pcoip.ki.agh.edu.pl>
2019-02-09 13:55:32 +0100

    v0.1

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,24 +10,31 @@ public abstract class Product {
 	private final BigDecimal taxPercent;
 
 	protected Product(String name, BigDecimal price, BigDecimal tax) {
+		if (name.isEmpty() || name == null || price == null || price.compareTo(new BigDecimal("0")) < 0)  {
+		    throw new IllegalArgumentException("Name is empty");
+		}
 		this.name = name;
 		this.price = price;
 		this.taxPercent = tax;
 	}
 
 	public String getName() {
-		return null;
+		return name;
 	}
 
 	public BigDecimal getPrice() {
-		return null;
+		return price;
 	}
 
 	public BigDecimal getTaxPercent() {
-		return null;
+		return taxPercent;
 	}
-
+	
 	public BigDecimal getPriceWithTax() {
-		return null;
+		return this.getPrice().add(this.getPrice().multiply(this.getTaxPercent()));
 	}
+	
+	
+
+
 }
